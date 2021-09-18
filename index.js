59b17e6e4a303aef43ef11e0b8ea9807ca078962
unknown <sv73107535@idat.edu.pe>
2021-09-18 09:23:44 -0500

    4.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function doTask() {
     let launching = new Promise((resolve) => {
         let listNumber = [];
         let intent = 10;
-        setTimeout(() => {while (intent > 0) {
+        while (intent > 0) {
             let number = Math.trunc(Math.random() * (7 - 1) + 1);
             if (number < 6) {
                 listNumber.push(number)
@@ -12,7 +12,7 @@ async function doTask() {
             intent --;
             resolve(listNumber);
         }
-        })})    
+        })   
     let results = await launching;
     for (let i = 1; i < results.length; i++) {
         console.log(`${i}° Launching, ${results[i]}`);
